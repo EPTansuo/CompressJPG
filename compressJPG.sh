67cbcd09af864a5c2bc1ec09c2933946ff6de2c6
eptansuo <eptansuo@163.com>
2020-08-25 09:35:48 +0800

    更改错误

--- compressJPG.sh
+++ compressJPG.sh
@@ -2,10 +2,7 @@
 
 mkdir outpath 
 
-find . -maxdepth 1 -size +1M  -iname "*.jpg" -print  -exec convert {} ./outpath/{} \;
-
-
-
-
+find . -maxdepth 1 -size +1M  -iname "*.jpg" -print  -exec convert -resize 50%x50% -quality 75%  {} ./outpath/{} \;
+find . -maxdepth 1 -size -1M  -iname "*.jpg" -print  -exec convert -quality 75% {} ./outpath/{} \; 
 
 
